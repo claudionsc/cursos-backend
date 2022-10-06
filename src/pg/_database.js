1851812556e38d51d4d24efdc366a0caf7a1dbd2
alvigabriel <claudio.alves20112011@hotmail.com>
2022-10-06 14:03:37 -0300

    teste3

--- src/pg/_database.js
+++ src/pg/_database.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DB_URI, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
     ssl: {
       rejectUnauthorized: false
