a52a7ec94505cf20dff810d348b02eae23843e1c
alvigabriel <claudio.alves20112011@hotmail.com>
2022-10-06 13:42:42 -0300

    teste2

--- src/pg/_database.js
+++ src/pg/_database.js
@@ -1,31 +1,14 @@
 const Sequelize = require('sequelize')
-const {Client} = require('pg')
 
-const sequelize = new Sequelize({
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  port: 5432,
-  logging: true,
-  ssl: true,
-  dialect: 'postgres',
+const sequelize = new Sequelize(process.env.DB_URI, {
   dialectOptions: {
-    "ssl": {"require":true,
-    "rejectUnauthorized": false, }
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 })
 
 module.exports = sequelize
-
-// const client = new Client({
-//   connectionString: process.env.DB_URI,
-//  ssl: {
-//     rejectUnauthorized: false,
-//   },
-// })
-
-// module.exports = client
 
 // Test DB Conenction //
 async function test(){
