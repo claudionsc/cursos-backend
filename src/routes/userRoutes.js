c8b23e551680a9bdfc4d16f61d8eb83420c8151a
alvigabriel <claudio.alves20112011@hotmail.com>
2022-12-07 11:04:26 -0300

    implementando sequelize, refatorando Database

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -10,21 +10,31 @@ app.use(cors())
 
 const userRoute = (app) => {
 
-    app.route('/cursos/:id?')
+    app.route('/cursos/:uuid?')
         .get(async (req, res) => {
-            const { id } = req.params
+            const uuid = req.params.uuid
             const query = {};
 
-            if (id){
-                query._id = id
-            }
+            if (uuid){
+                try {
+                    const curso = await CursosModels.findOne({ where: { id: uuid}})
+                  
+                    if (curso === null){
+                        res.send('Curso não encontrado')
+                    } else {res.send({ curso })}
+                }  catch (error){
+                    res.status(400).send({ error: 'Falha ao encontrar cursos'})
+                }
+            
+            }else{
 
-            try {
-                const cursos = await CursosModels.findAll(query)
-                res.send({ cursos })
-
-            } catch (error){
-                res.status(400).send({ error: 'Falha ao encontrar curso' })
+                try {
+                    const cursos = await CursosModels.findAll(query)
+                    res.send({ cursos })
+    
+                } catch (error){
+                    res.status(400).send({ error: 'Falha ao encontrar curso' })
+                }
             }
         })
         .post(async (req, res) => {
